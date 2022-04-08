54770d0ab99c0cad8e513645871f92a8c86d2b82
RafiRide123 <>
2022-04-09 05:30:00 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul7_1302204110/BankTransferConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_1302204110
+{
+    internal class BankTransferConfig
+    {
+    }
+}
